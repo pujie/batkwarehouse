3586aefe2cfb48730d2d7250d158fb328cde36c3
pujie <pw.prayitno@gmail.com>
2020-01-14 21:40:35 +0700

    first files

---
+++ application/controllers/Gudang.php
@@ -0,0 +1,31 @@
+<?php
+
+class Gudang extends CI_Controller {
+    function __construct(){
+        parent::__construct();
+    }
+    function index(){
+        $this->load->view('main');
+    }
+    function getparam(){
+        $param1=$this->uri->segment(3);
+        $param2=$this->uri->segment(4);
+        $param3=$this->uri->segment(5);
+        $param4=$this->uri->segment(6);
+        if($param1=='ngetes'){
+            echo 'ini cuma ngetes ';
+        }
+        else{
+            echo 'ini serius <br/>
+            tapi boong LOL ';
+        }
+        echo $param1;
+        echo $param2;
+        echo $param3*$param4;
+    }
+    function penjumlahan(){
+        $angka1=$this->uri->segment(3);
+        $angka2=$this->uri->segment(4);
+        echo $angka1+$angka2;
+    }
+}
