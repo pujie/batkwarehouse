46bdaf5bf830d08e8339303d73efd1a99366eecc
pujie <pw.prayitno@gmail.com>
2020-02-05 03:13:44 +0700

    fix total

--- application/views/sales/kasir.php
+++ application/views/sales/kasir.php
@@ -84,7 +84,7 @@
 						<!-- BEGIN EXAMPLE TABLE PORTLET-->
 						<div class="portlet box blue">
 							<div class="portlet-title">
-								<div class="caption"><i class="icon-edit"></i>Sales</div>
+								<div class="caption"><i class="icon-edit"></i>Kasir</div>
 								<div class="tools">
 									<a href="javascript:;" class="reload"></a>
 								</div>
@@ -95,7 +95,7 @@
 										<a href="#addbuy" data-toggle="modal" class="btn red">Add</a>
 									</div>
 									<div class="btn-group pull-right">
-										<button class="btn dropdown-toggle" data-toggle="dropdown">Tools <i class="icon-angle-down"></i>
+										<button style="display:none;" class="btn dropdown-toggle" data-toggle="dropdown">Tools <i class="icon-angle-down"></i>
 										</button>
 										<ul class="dropdown-menu pull-right">
 											<li><a href="#">Print</a></li>
@@ -107,22 +107,38 @@
 								<table class="table table-striped table-hover table-bordered" id="sample_editable_1x">
 									<thead>
 										<tr>
-											<th>No</th>
-											<th>Nama</th>
-											<th>Harga</th>
-											<th>Jumlah</th>
-											<th>Total</th>
-											<th>Delete</th>
+											<th width="10%">No</th>
+											<th width="45%">Nama</th>
+											<th width="15%">Harga</th>
+											<th width="15%">Jumlah</th>
+											<th width="15%">Total</th>
+											<th width="10%">Delete</th>
 										</tr>
 									</thead>
 									<tbody>
 									</tbody>
+									<tfoot>
+									<tr>
+									<td colspan=4 width="85%">Total</td><td width="85%"><span id="total">0</span></td><td width="10%"></td>
+									</tr>
+									</tfoot>
 								</table>
 								<table class="table table-striped table-hover table-bordered">
 									<tr>
-									<td>Total</td><td><span id="total">0</span></td>
+									<td colspan=4 width="85%">Cash</td><td width="85%"><span id="cash">0</span></td><td width="10%"></td>
+									</tr>
+									<tr>
+									<td colspan=4 width="85%">return</td><td width="85%"><span id="return">0</span></td><td width="10%"></td>
 									</tr>
 								</table>
+								<div class="table-toolbar">
+									<div class="btn-group">
+
+									</div>
+									<div class="btn-group pull-right">
+										<button class="btn green">Bayar</button>
+									</div>
+								</div>
 							</div>
 						</div>
 						<!-- END EXAMPLE TABLE PORTLET-->
@@ -176,28 +192,45 @@
 			   lengthadd = 0;
 			   lengthadd = amount + 1;
 			   temp = $('#productprice').val()*$('#productamount').val();
-			   str = '<tr id=3>';
+			   str = '<tr tot='+temp+'>';
 			   str+= '<td class="">'+lengthadd+'</td>';
 			   str+= '<td>'+$('#productid').val()+'</td>';
 			   str+= '<td>'+numberWithCommas($('#productprice').val())+'</td>';
 			   str+= '<td>'+$('#productamount').val()+'</td>';
 			   str+= '<td class="center">'+numberWithCommas($('#productprice').val()*$('#productamount').val())+'</td>';
-			   str+= '<td><a class="deleteRow" href="javascript:;">Delete</a></td>';
+			   str+= '<td><button class="btn deleteRow" onclick="deleteRow($(this))">Delete</button></td>';
 			   str+= '</tr>';
 			   console.log("Invoked");
 			   mytotal*=1;
 			   mytotal = mytotal+temp;
 			   $("#total").html(numberWithCommas(mytotal));
 			   $("#sample_editable_1x").prepend(str);
-			   $(".deleteRow").on("click",function(){
+			   $(".deleteRowx").on("click",function(){
 				console.log($(this));
+				tr = $(this).parent().parent();
+				tot = tr.attr('tot')*1;
+				console.log("Tot",tot);
+				console.log("MyTotal",mytotal);
+				mytotal*=1;
+				mytotal = mytotal-tot;
+				$("#total").html(numberWithCommas(mytotal));
+				$(this).parent().parent().remove();
+				$(this).off('click');  
 				})
 		   });
 		   $(".closedialog").on("click",function(){});
-			$(".deleteRow").on("click",function(){
-				console.log($(this));
-			})
+			deleteRow = function(par){
+				console.log("Par",par);
 
+				tr = par.parent().parent();
+				tot = tr.attr('tot')*1;
+				console.log("Tot",tot);
+				console.log("MyTotal",mytotal);
+				mytotal*=1;
+				mytotal = mytotal-tot;
+				$("#total").html(numberWithCommas(mytotal));
+				par.parent().parent().remove();
+			}
 		   function numberWithCommas(x) {
 				var parts = x.toString().split(",");
 				parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
